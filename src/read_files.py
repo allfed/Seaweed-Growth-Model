dddf41f20f6e608a06b4c1bbd2b4493e0a6ac65f
Florian Ulrich Jehn <florianjehn@posteo.de>
2022-07-15 11:28:31 +0200

    test

---
+++ src/read_files.py
@@ -0,0 +1,8 @@
+def create_lognormal_distribution():
+    """
+    Creates a log-normal distribution between 1 and 100
+    and returns it as a pandas dataframe
+    """
+    import numpy as np
+    import pandas as pd
+    return pd.DataFrame(np.random.lognormal(0, 1, 100), columns=['value'])
