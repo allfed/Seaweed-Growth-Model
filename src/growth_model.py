51f842c37530a6f2bb0c21ec7ca207267ff31520
Florian Ulrich Jehn <florianjehn@posteo.de>
2022-07-19 11:06:10 +0200

    Finished the nutrient factor

--- src/growth_model.py
+++ src/growth_model.py
@@ -5,6 +5,9 @@ James, S.C. and Boriah, V. (2010), Modeling algae growth
 in an open-channel raceway
 Journal of Computational Biology, 17(7), 895−906.
 """
+import math
+
+
 def growth_factor_combination(opt_growth_rate, non_opt_illumniation, non_opt_temperature,
                             non_opt_nutrients, non_opt_salinity, self_shading):
     """
@@ -42,4 +45,48 @@ def calculate_illumination_factor(illumination):
         return 100 / illumination
     else:
         return 1
-    
+
+
+def calculate_temperature_factor(temperature):
+    """
+    Calculates the temperature factor
+    Arguments:
+        temperature: the temperature of the water in °C
+    Returns:
+        The temperature factor
+    """
+    # where Kt1 = 0.017°C−2 and Kt2 = 0.06°C−2. These coefficients were determined by 
+    # fitting the preceding equation such that g(15°C) = 0.25 and g(36°C) = 0.1
+    Kt1 = 0.017
+    Kt2 = 0.06
+
+    if temperature < 24:
+        return math.exp(-Kt1 * (24 - temperature)**2)
+    elif temperature > 30:
+        return math.exp(-Kt2 * (temperature - 30)**2)
+    else:
+        return 1
+
+def calculate_nutrient_factor(nitrate, ammonium, phosphate):
+    """
+    Calculates the nutrient factor, which is the minimum of the three nutrients
+    Arguments:
+        nitrate: the nitrate concentration in mg/L
+        ammonium: the ammonium concentration in mg/L
+        phosphate: the phosphate concentration in mg/L
+    Returns:
+        The nutrient factor
+    """
+    # where KNO3 = 0.4 μM, KNH4 = 0.3 μM, and KPO4 = 0.1 μM.
+    # were implemented because these yield growth rates consistent with  observations by Lapointe [1987]
+    # "Phosphorus- and nitrogen-limited photosynthesis and growth of Gracilaria tikvahiae (Rhodophyceae)
+    # in the Florida Keys: an experimental field study"
+    KNO3 = 0.4
+    KNH4 = 0.3
+    KPO4 = 0.1
+    # Calculate the single nutrient factors
+    nitrate_factor = nitrate / (KNO3 + nitrate)
+    ammonium_factor = ammonium / (KNH4 + ammonium)
+    phosphate_factor = phosphate / (KPO4 + phosphate)
+    # Calculate the nutrient factor
+    return min(nitrate_factor, ammonium_factor, phosphate_factor)
--- tests/test_growth_model.py
+++ tests/test_growth_model.py
@@ -11,6 +11,7 @@ sys.path.append(str(path_root))
 
 from src.growth_model import growth_factor_combination
 from src.growth_model import calculate_illumination_factor
+from src.growth_model import calculate_temperature_factor
 
 def test_growth_factor_combination():
     """
@@ -39,4 +40,23 @@ def test_calculate_illumination_factor():
     assert calculate_illumination_factor(500) == 100/500
     # Test 3
     assert calculate_illumination_factor(5) == 5/21.9
-   
+    # Test 4: make sure everything stays between 0 and 1
+    for illumination in range(0, 500):
+        assert calculate_illumination_factor(illumination) <= 1
+        assert calculate_illumination_factor(illumination) >= 0
+
+
+def test_calculate_temperature_factor():
+    """
+    Tests the calculate_temperature_factor function
+    """
+    # Test 1: make sure everything stays between 0 and 1
+    for temperature in range(0, 50):
+        assert calculate_temperature_factor(temperature) <= 1
+        assert calculate_temperature_factor(temperature) >= 0
+
+    # Test 2: temperature between 24 and 30
+    assert calculate_temperature_factor(25) == 1
+    # Test 3: temperature above 30
+
+    
