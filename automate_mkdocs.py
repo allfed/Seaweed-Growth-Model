3a1283a30347e74f5d0117c69961e1b017506403
Florian Ulrich Jehn <florianjehn@posteo.de>
2022-08-23 14:29:56 +0200

    Update automate_mkdocs.py

--- automate_mkdocs.py
+++ automate_mkdocs.py
@@ -2,7 +2,8 @@
 
 import ast
 import importlib
-import json, yaml
+import json
+import yaml
 from collections import defaultdict
 from pathlib import Path
 from typing import Union
@@ -23,14 +24,14 @@ def automate_mkdocs_from_docstring(
     repo_dir: Path,
     match_string: str
 ) -> dict:
-    """Automates the -pages for mkgendocs package by adding all Python functions in a 
+    """Automates the -pages for mkgendocs package by adding all Python functions in a
     directory to the mkgendocs config.
     Args:
-        mkdocs_dir (typing.Union[str, pathlib.Path]): textual directory for 
+        mkdocs_dir (typing.Union[str, pathlib.Path]): textual directory for
         the hierarchical directory & navigation in Mkdocs
         mkgendocs_f (str): The configurations file for the mkgendocs package
         repo_dir (pathlib.Path): textual directory to search for Python functions in
-        match_string (str): the text to be matches, after which the functions will be 
+        match_string (str): the text to be matches, after which the functions will be
         added in mkgendocs format
     Example:
         >>>
@@ -135,14 +136,14 @@ def automate_nav_structure(
     match_string: str,
     structure: dict,
 ) -> str:
-    """Automates the -pages for mkgendocs package by adding all Python 
+    """Automates the -pages for mkgendocs package by adding all Python
     functions in a directory to the mkgendocs config.
     Args:
-        mkdocs_dir (typing.Union[str, pathlib.Path]): textual directory for 
+        mkdocs_dir (typing.Union[str, pathlib.Path]): textual directory for
         the hierarchical directory & navigation in Mkdocs
         mkgendocs_f (str): The configurations file for the mkgendocs package
         repo_dir (pathlib.Path): textual directory to search for Python functions in
-        match_string (str): the text to be matches, after which the functions 
+        match_string (str): the text to be matches, after which the functions
         will be added in mkgendocs format
     Example:
         >>>
