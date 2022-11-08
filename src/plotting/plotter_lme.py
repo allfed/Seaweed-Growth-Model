bfe07e33439ad18a0efd5fb2376963914928442b
Florian Ulrich Jehn <florianjehn@posteo.de>
2022-11-08 12:18:38 +0100

    Redid lme plots

--- src/plotting/plotter_lme.py
+++ src/plotting/plotter_lme.py
@@ -1,138 +1,77 @@
-from src.model.seaweed_model import SeaweedModel
-import geopandas as gpd
-import matplotlib.pyplot as plt
+import os
 import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import numpy as np
 # Import the ALLFED stle
 plt.style.use(
     "https://raw.githubusercontent.com/allfed/ALLFED-matplotlib-style-sheet/main/ALLFED.mplstyle"
 )
 
 
-class PlotterLME:
+def cluster_timeseries_all_parameters_q_lines(parameters, lme, lme_dict):
     """
-    Class to organize all the plotting functions
+    Plots line plots for all clusters and all parameters
+    Arguments:
+        parameters: a dictionary of dataframes of all parameters
+    Returns:
+        None, but saves the plot
     """
+    fig, axes = plt.subplots(
+        nrows=5, ncols=1, sharey=True, sharex=True, figsize=(20, 20)
+    )
+    i = 0
+    for parameter, parameter_df in parameters.items():
+        ax = axes[i]
+        lme_df = parameter_df.loc[lme, :]
+        lme_df.plot(kind="line", ax=ax, legend=False)
+        ax.set_ylabel(parameter)
+        ax.set_xlabel("Months since nuclear war")
+        i += 1
 
-    def __init__(self, seaweed_model):
-        self.seaweed_model = seaweed_model
+    plt.savefig(
+        "results" + os.sep + "lme" + os.sep + "cluster_timeseries_all_param_q_lines_LME_"
+        + lme_dict[lme] + ".png",
+        dpi=200,
+        bbox_inches="tight",
+    )
+    plt.close()
 
-    def plot_growth_rate_by_lme_bar(self, month, path=""):
-        """
-        Plots the growth rate for the model based on LME as a bar chart
-        """
-        assert self.seaweed_model.lme_or_grid == "lme"
-        date_section_df = self.seaweed_model.construct_df_from_sections_for_date(month)
-        ax = date_section_df.seaweed_growth_rate.sort_values().plot(kind="bar")
-        ax.set_title("Growth Rate by LME")
-        ax.set_xlabel("LME")
-        ax.set_ylabel("Fraction of optimal growth rate")
-        fig = plt.gcf()
-        fig.set_size_inches(10, 5)
-        plt.savefig(
-            path + "growth_rate_by_lme_bar" + str(month) + ".png",
-            dpi=200,
-            bbox_inches="tight",
+
+def create_name_dict():
+    """
+    Creates a lookup dictionary for the LME names
+    Returns:
+        A dictionary with LME names
+    """
+    lme_df = pd.read_csv(
+        "data" + os.sep + "geospatial_information" + os.sep + "lme_shp"
+        + os.sep + "lme_metadata.csv"
+    )
+    return dict(zip(lme_df.LME_NUMBER, lme_df.LME_NAME))
+
+
+def main():
+    parameters = {}
+    parameter_names = [
+        "salinity_factor", "nutrient_factor",
+        "illumination_factor", "temp_factor", "seaweed_growth_rate"
+    ]
+    for parameter in parameter_names:
+        parameters[parameter] = pd.DataFrame(
+            pd.read_pickle(
+                "data" + os.sep + "interim_results" + os.sep
+                + parameter + "_LME.pkl"
+            )
         )
-        plt.close()
+    lme_dict = create_name_dict()
+    for lme in range(1, 67):
+        cluster_timeseries_all_parameters_q_lines(parameters, lme, lme_dict)
 
-    def plot_growth_rate_by_lme_global(self, month, path=""):
-        """
-        Plots the growth rate fraction for all LME on a global map
-        """
-        assert self.seaweed_model.lme_or_grid == "lme"
-        date_section_df = self.seaweed_model.construct_df_from_sections_for_date(month)
-        lme_shape = gpd.read_file("data/geospatial_information/lme_shp/lme66.shp")
-        lme_global = lme_shape.merge(
-            date_section_df, left_on="LME_NUMBER", right_index=True
-        )
-        ax = lme_global.plot(
-            "seaweed_growth_rate",
-            edgecolor="black",
-            linewidth=0.1,
-            vmin=0,
-            vmax=0.8,
-            cmap="Greens",
-            legend=True,
-        )
-        ax.set_title(
-            "Fraction of optimal growth rate in month after nuclear war: " + str(month)
-        )
 
-        fig = plt.gcf()
-        fig.set_size_inches(10, 5)
-        plt.savefig(
-            path + "growth_rate_by_lme_global_" + str(month) + ".png",
-            dpi=200,
-            bbox_inches="tight",
-        )
-        plt.close()
+if __name__ == "__main__":
+    main()
 
-    def calculate_mean_groth_rate_by_lme(self):
-        """
-        Calculates the mean growth rate for a LME for the whole
-        time period modelled.
-        Arguments:
-            None
-        Returns:
-            None
-        """
-        assert self.seaweed_model.lme_or_grid == "lme"
-        growth_rate_dict = {}
-        for section_name, section_instance in self.seaweed_model.sections.items():
-            growth_rate_dict[
-                section_name
-            ] = section_instance.calculate_mean_growth_rate()
-        growth_df = pd.DataFrame.from_dict(growth_rate_dict, orient="index")
-        growth_df.columns = ["mean_growth_rate"]
-        print("LMEs with highest mean growth rates fraction are:")
-        print(growth_df["mean_growth_rate"].sort_values(ascending=False).head())
-        print(
-            "Global mean growth rate fraction over the whole time period:"
-            + str(growth_df["mean_growth_rate"].mean())
-        )
-
-    def plot_growth_rate_by_best_lme_as_line(self, path="", window=10):
-        """
-        Takes the growthrate of the 3 best LMEs (by mean growth rate)
-        29    0.391329
-        11    0.309920
-        38    0.304329
-        and plots them over time.
-
-        Arguments:
-            path: the path to save the plot to
-            window: the window size for the rolling mean
-
-        Returns:
-            None
-        """
-        assert self.seaweed_model.lme_or_grid == "lme"
-        best_lme = [29, 11, 38]
-        best_lme_dict = {
-            29: "Benguela Current",
-            11: "Pacific Central-American Coastal",
-            38: "Indonesian Sea",
-        }
-        ax = plt.gca()
-        df_list = []
-        for section_name, section_instance in self.seaweed_model.sections.items():
-            if section_name in best_lme:
-                temp_df = (
-                    section_instance.section_df["seaweed_growth_rate"]
-                    .rolling(window=window)
-                    .mean()
-                    .dropna()
-                )
-                temp_df.name = best_lme_dict[section_name]
-                df_list.append(temp_df)
-        mean_df = pd.concat(df_list, axis=1)
-        mean_df.plot(ax=ax, alpha=0.8)
-        ax.set_title("Smoothed Growth Rate of the 3 most productive LMEs")
-        ax.set_ylabel("Fraction of optimal growth rate")
-        fig = plt.gcf()
-        fig.set_size_inches(10, 5)
-        plt.savefig(path + "growth_rate_by_best.png", dpi=200, bbox_inches="tight")
-        plt.close()
 
 
 
